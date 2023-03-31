9da3c746ea4832e75a99a44c0959cb380d8a50d8
H4yt4m <haytam.elmerabeti@gmail.com>
2023-03-31 14:48:38 +0200

    ♻️ Refactor ising

--- src/ising_model.py
+++ src/ising_model.py
@@ -12,6 +12,7 @@ import json
 import math
 import matplotlib.animation as animation
 
+# Parse arguments
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Get experiment values from parameters file.')
     if sys.argv[1] == "--generate" :
@@ -19,7 +20,7 @@ def parse_arguments():
         args = parser.parse_args()
         parameters_file_path = json.load(open(args.generate))
         return parameters_file_path   
-    # elif sys.argv[1] == some other mode ?
+    # elif sys.argv[1] == Other modes ?
     else :
         print("\nMode not recognized.")
 
@@ -29,6 +30,7 @@ def initialise_config(lattice_size):
 def get_neighbours(config, x, y):
     return [config[x+1,y], config[x-1,y], config[x,y+1], config[x,y-1]]
 
+# Local energy of a given pixel with respect to a class c
 def calculate_local_energy(pixel_proba):
     return -np.log((1/pixel_proba)-1)/4
 
@@ -39,11 +41,14 @@ def calculate_hamiltonian(lattice, lattice_size, beta):
     external_field_term = 0
     coupling_term = 0
 
+    # Todo : Replace by pixels probabilities from SVM
+    pixel_proba = 0.99
+
     # Energy from the total magnetic field present
     for i in range(lattice_size):
         for j in range(lattice_size):
             # get one spin at a time from all spins in the lattice
-            external_field_term += -calculate_local_energy(0.7) * lattice[i, j]
+            external_field_term += -calculate_local_energy(pixel_proba) * lattice[i, j]
 
     # Energy from the neighbors
     for i in range(1, lattice_size-1):
@@ -59,14 +64,16 @@ def calculate_hamiltonian(lattice, lattice_size, beta):
 
     return external_field_term + coupling_term
 
+# Cost of spin flip
 def state_transition_probability(delta_hamiltonians, beta):
-    return np.exp(-delta_hamiltonians*beta)
+    return np.exp(-beta * delta_hamiltonians)
 
-def update(nb_candidate_pixels_per_iteration):
+# Simulation function
+def simulate(nb_candidate_pixels_per_iteration):
     global config
     lattice_size = len(config[0])
-    for _ in range(nb_candidate_pixels_per_iteration):
 
+    for _ in range(nb_candidate_pixels_per_iteration):
         # Pick a random spin
         spin_x = np.random.randint(0, lattice_size)
         spin_y = np.random.randint(0, lattice_size)
@@ -76,14 +83,13 @@ def update(nb_candidate_pixels_per_iteration):
         candidate_config[spin_x, spin_y] *= -1
 
         # Calculate energy change between the two configs
-        delta_hamiltonians = 0 # ??
-        delta_hamiltonians = calculate_hamiltonian(candidate_config, lattice_size, beta) - calculate_hamiltonian(config, lattice_size, beta) 
+        delta_hamiltonians = calculate_hamiltonian(candidate_config, lattice_size, beta) -\
+            calculate_hamiltonian(config, lattice_size, beta) 
 
         # Test whether we accept the flip or not
         if delta_hamiltonians < 0 :
-        # If delta_hamiltonians < 0 or np.random.randint(0, 1) < state_transition_probability(delta_hamiltonians, beta):
-
-            config[spin_x, spin_y] *= -1
+        # if np.random.randint(0, 1) < state_transition_probability(delta_hamiltonians, beta):
+            config[spin_x, spin_y] *= -1    
 
 # Visualisation
 def init():
@@ -91,31 +97,32 @@ def init():
     return im,
 
 def animate(i):
-    update(1)
+    simulate(1)
     im.set_data(config)
     return im,
 
-
-# Parse experiment parameters
 try:
+    # Parse experiment parameters
     args = parse_arguments()
     lattice_size = args.get("lattice_size")
     J = args.get("J")
     max_iterations = args.get("max_iterations")
     beta = args.get("beta")
+    s = args.get("seed")
+
+    np.random.seed(s)
 
 except:
     print("\nPlease choose an execution mode :\n -> For generating a new Ising model : --generate PARAMETERS_FILE_PATH \n -> For ???")
 
 try:
+    # Visualize lattice
     fig = plt.figure()
-
     config = initialise_config(lattice_size)
-
-    im = plt.imshow(config, vmin=0, vmax=1)
-
-    anim = animation.FuncAnimation(fig, animate, frames=60, interval=1)
-
+    im = plt.imshow(config)
+    anim = animation.FuncAnimation(fig, animate, frames=200, interval=1, blit=True)
     plt.show()
+    # anim.save('ising_model.gif')
+
 except:
     print("\nError visualizing Ising model.")
