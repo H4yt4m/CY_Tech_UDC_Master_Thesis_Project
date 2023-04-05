57dc84570391bf02c34a4e33f1d3a4782e4599f4
H4yt4m <haytam.elmerabeti@gmail.com>
2023-04-05 16:03:24 +0200

    🔧 Add normalisation to SVM

--- src/job_svm.sh
+++ src/job_svm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH -J python_job            # Job name
-#SBATCH -o python_job_%j.o       # Name of stdout output file(%j expands to jobId)
-#SBATCH -e python_job_%j.e       # Name of stderr output file(%j expands to jobId)
-#SBATCH -c 1                     # Cores per task requested
-#SBATCH -t 00:10:00              # Run time (hh:mm:ss)
-#SBATCH --mem-per-cpu=1G         # Memory per core demandes
+#SBATCH -J svm_job            # Job name
+#SBATCH -o svm_job_%j.o       # Name of stdout output file(%j expands to jobId)
+#SBATCH -e svm_job_%j.e       # Name of stderr output file(%j expands to jobId)
+#SBATCH -c 1                  # Cores per task requested
+#SBATCH -t 00:10:00           # Run time (hh:mm:ss)
+#SBATCH --mem-per-cpu=1G      # Memory per core demandes
 
 module load cesga/2020
 python3 ./svm.py
--- src/svm.py
+++ src/svm.py
@@ -17,6 +17,8 @@ from sklearn.svm import SVC
 from scipy.io import loadmat
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
+import numpy as np
+from scipy.stats import norm
 
 # !wget http://www.ehu.eus/ccwintco/uploads/6/67/Indian_pines_corrected.mat http://www.ehu.eus/ccwintco/uploads/c/c4/Indian_pines_gt.mat
 
@@ -38,9 +40,13 @@ def extract_pixels_one_vs_all_classes(X, y, class_number):
     df.to_csv('Dataset.csv')
     return df, np.reshape(temp, (145,145))
 
-def normalize_data(data):
-    # todo
-    return normalized_data
+def normalize_data(df):
+    # Remove labels class
+    temp = df.loc[:, df.columns != 'class']
+    # Apply Z-Score normalisation
+    temp = temp.apply(lambda column : (column-norm.fit(column)[0])/norm.fit(column)[1], axis=0)
+    df_norm = pd.concat([temp, df['class']], axis=1)
+    return df_norm
 
 def split_into_train_test(df, test_percentage):
     x = df[df['class'] != 0]
@@ -75,10 +81,13 @@ def main():
     
     start = time.time()
     
-    X, y = read_files('./Indian_pines_corrected.mat', './Indian_pines_gt.mat')
+    X, gt = read_files('./Indian_pines_corrected.mat', './Indian_pines_gt.mat')
     
     # One-vs-All classification
-    df, y = extract_pixels_one_vs_all_classes(X, y, 11)
+    df, ground_truth = extract_pixels_one_vs_all_classes(X, gt, 11)
+    
+    # Z-Score normalisation
+    df = normalize_data(df)
     
     X_train, X_test, y_train, y_test = split_into_train_test(df, 0.75)
 
@@ -118,7 +127,7 @@ def main():
     print(probabilities[0:3])
     
     print("\n Ground Truth : \n")
-    plot_image(y, 'IP_GT.png')
+    plot_image(ground_truth, 'IP_GT.png')
     visualise_prediction(df, svm)
     
 ######################################################################################
