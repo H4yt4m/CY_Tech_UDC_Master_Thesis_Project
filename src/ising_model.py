5cba60f0de7d0f679066b6f27594b50ca2a98acf
H4yt4m <haytam.elmerabeti@gmail.com>
2023-04-06 11:33:50 +0200

    ♻️ Refactor ising model

--- src/ising_model.py
+++ src/ising_model.py
@@ -27,19 +27,23 @@ def parse_arguments():
     else :
         print("\nMode not recognized.")
 
-def initialise_config(lattice_size, probabilities):
+def initialise_config(lattice_size, probabilities=None):
     # # Option 1 : Random initialisation
-    # return 2*np.random.randint(2, size=(lattice_size,lattice_size))-1
+    # lattice = 2*np.random.randint(2, size=(lattice_size,lattice_size))-1
     
     # Option 2 : Initialisation based on threshholding from the prbabilities
-    config = np.zeros(shape=(lattice_size, lattice_size))
-    for i in range(lattice_size):
-        for j in range(lattice_size):
-            config[i][j] = -1 if probabilities[i*lattice_size+j][0] < 0.5 else 1
-    return config
+    if probabilities is None :
+        print("\n\nPlease specify the file containing the probabilities to initialise the lattice.\n\n")
+    else :
+        lattice = np.zeros(shape=(lattice_size, lattice_size))
+        for i in range(lattice_size):
+            for j in range(lattice_size):
+                lattice[i][j] = -1 if probabilities[i*lattice_size+j][0] < 0.5 else 1
 
-def get_neighbours(config, x, y):
-    return [config[x+1,y], config[x-1,y], config[x,y+1], config[x,y-1]]
+    return lattice
+
+def get_neighbours(lattice, x, y):
+    return [lattice[x+1,y], lattice[x-1,y], lattice[x,y+1], lattice[x,y-1]]
 
 # Local energy of a given pixel with respect to a class c
 def calculate_local_energy(pixel_proba):
@@ -79,20 +83,18 @@ def state_transition_probability(delta_hamiltonians, beta):
 
 # To verify
 def get_pixel_ising_proba(list_lowest_energy_configurations, pixel_x, pixel_y):
-    canonical_partition = 0
     K = len(list_lowest_energy_configurations)
 
-    # Normalization constant
+    # Canonical partition, ie. normalization constant
+    canonical_partition = 0
     for eps in range (K+1):
-        config = list_lowest_energy_configurations[eps]
-        canonical_partition += np.exp(-calculate_hamiltonian(config, beta, probabilities))
-
-    pixel_proba = 1/canonical_partition
+        lattice = list_lowest_energy_configurations[eps]
+        canonical_partition += np.exp(-calculate_hamiltonian(lattice, beta, probabilities))
 
     # Pixel probability
     for k in range (K+1):
-        config = list_lowest_energy_configurations[k]
-        spin = config[pixel_x][pixel_y]
+        lattice = list_lowest_energy_configurations[k]
+        spin = lattice[pixel_x][pixel_y]
         temp += (1 if spin==1 else 0)/np.exp(calculate_hamiltonian(list_lowest_energy_configurations[k], beta, probabilities))
 
     pixel_proba = temp / canonical_partition
@@ -100,12 +102,12 @@ def get_pixel_ising_proba(list_lowest_energy_configurations, pixel_x, pixel_y):
     return pixel_proba
 
 # Simulation function
-def simulate(i, nb_candidate_pixels_per_iteration):
-    global config, probabilities
+def update_figure(i, nb_candidate_pixels, probabilities, beta):
+
     lattice_size = len(config[0])
     flipped = False
 
-    for _ in range(nb_candidate_pixels_per_iteration):
+    for _ in range(nb_candidate_pixels):
         # Pick a random spin
         spin_x = np.random.randint(0, lattice_size)
         spin_y = np.random.randint(0, lattice_size)
@@ -126,49 +128,63 @@ def simulate(i, nb_candidate_pixels_per_iteration):
 
     return im,
 
-try:
-    # Parse experiment parameters
-    args = parse_arguments()
-    lattice_size = args.get("lattice_size")
-    J = args.get("J")
-    max_iterations = args.get("max_iterations")
-    beta = args.get("beta")
-    s = args.get("seed")
-    nb_candidate_pixels_per_iteration = args.get("nb_candidate_pixels_per_iteration")
-    visualize_or_save = args.get("visualize_or_save")
+
+def main():
+    # Configuration and visualization image instances should be accessible by update_fig
+    global config, im
     
-    np.random.seed(s)
+    try:
+        # Parse experiment parameters
+        args = parse_arguments()
+        lattice_size = args.get("lattice_size")
+        max_iterations = args.get("max_iterations")
+        beta = args.get("beta")
+        s = args.get("seed")
+        nb_candidate_pixels = args.get("nb_candidate_pixels")
+        visualize_or_save = args.get("visualize_or_save")
+        
+        np.random.seed(s)
 
-except:
-    print("\nPlease choose an execution mode :\n -> For generating a new Ising model : --generate PARAMETERS_FILE_PATH \n -> For ???")
+    except:
+        print("\nPlease choose an execution mode :\n -> For generating a new Ising model : --generate PARAMETERS_FILE_PATH \n -> For ???")
 
-# Initiate matplotlib elements
-fig = plt.figure()
-probabilities = np.loadtxt("probabilities.txt", dtype=float, delimiter=" ")
-config = initialise_config(lattice_size, probabilities)
-im = plt.imshow(config)
+    # Initiate matplotlib elements
+    fig = plt.figure()
+    probabilities = np.loadtxt("probabilities.txt", dtype=float, delimiter=" ")
+    config = initialise_config(lattice_size, probabilities)
+    im = plt.imshow(config)
 
-anim = animation.FuncAnimation(
-    fig,
-    partial(simulate, nb_candidate_pixels_per_iteration = nb_candidate_pixels_per_iteration),
-    frames=max_iterations,
-    interval=1,
-    blit=True,
-    repeat=False
-)
+    anim = animation.FuncAnimation(
+        fig,
+        partial(update_figure,
+                    nb_candidate_pixels = nb_candidate_pixels,
+                    probabilities = probabilities,
+                    beta = beta
+                ),
+        frames=max_iterations,
+        interval=1,
+        blit=True,
+        repeat=False
+    )
 
-# Visualize lattice interactively
-if visualize_or_save == "visualize" :
-    plt.show()
+    # Visualize lattice interactively
+    if visualize_or_save == "visualize" :
+        plt.show()
 
-# Save animation as mp4 file
-elif visualize_or_save == "save" :
-    # Format : gif
-    f = r"./ising_model.gif" 
-    writergif = animation.PillowWriter(fps=60) 
-    anim.save(f, writer=writergif)
-    
-    # # Format : mp4
-    # f = r"./ising_model.mp4" 
-    # writervideo = animation.FFMpegWriter(fps=60) 
-    # anim.save(f, writer=writervideo)
+    # Save animation as mp4 file
+    elif visualize_or_save == "save" :
+
+        # Format : gif
+        f = r"./ising_model.gif" 
+        writergif = animation.PillowWriter(fps=60) 
+        anim.save(f, writer=writergif)
+        
+        # # Format : mp4
+        # f = r"./ising_model.mp4" 
+        # writervideo = animation.FFMpegWriter(fps=60) 
+        # anim.save(f, writer=writervideo)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
